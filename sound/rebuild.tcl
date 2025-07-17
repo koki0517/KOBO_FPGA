ac6d63174d79ab0393d22f0f1efc6df11e89d8fe
koki0517 <koki1743ok@gmail.com>
2025-07-17 19:50:43 +0900

    クソデカIPの削除

--- sound/rebuild.tcl
+++
@@ -1,3 +0,0 @@
-remove_files -fileset sources_1 [get_files -norecurse *]
-add_files -fileset sources_1 [list /home/koki0/KOBO_FPGA/sound/sound.v /home/koki0/KOBO_FPGA/sound/soundtop.v /home/koki0/KOBO_FPGA/sound/uart.v]
-update_compile_order -fileset sources_1
